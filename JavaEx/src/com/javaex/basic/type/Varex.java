473647d2fefb14e56e4a51f5e3f6a76089f93841
조성현 <happy1433@naver.com>
2021-02-25 16:08:27 +0900

    변수 선언과 초기화 조회

---
+++ JavaEx/src/com/javaex/basic/type/Varex.java
@@ -0,0 +1,32 @@
+package com.javaex.basic.type;
+
+// 변수 연습
+public class Varex {
+	public static void main (String[] args) {
+		// 변수 식별자 규정
+		// 문자, 숫자, $, _의 조합으로 생성
+		// 숫자로 시작하면 안됨
+		// 예약어는 사용할 수 없다
+		
+		// 자바 변수, 메서드명관례:
+		//	   소문자로 작성, 단어의 조합일 경우, 뒷 단어의 첫글자는 대문자(camelCase)
+		
+		//초기화
+	
+		int myAge; // int 타입 데이터를 담을 공간을 할당
+		myAge = 33; // 초기화, 할당
+		
+		System.out.println("내 나이는: " +myAge); //
+		
+		// Java는 정적 타입 언어
+		// 한번 선언되면 타입을 변경할 수 없다
+//		myAge = "문자열";
+		
+		System.out.println("내 나이는:" + myAge); //조회
+		
+		// 한 번에 여러 변수를 선언할 때
+		int v1=10, v2=20, v3=30;
+		
+	}
+
+}
